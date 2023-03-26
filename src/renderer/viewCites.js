1cc2180bdc9be13efb67279685a59992f00a4ded
Juan Esteban Castro Molano <juanescastro29@gmail.com>
2023-03-26 15:26:56 -0500

    view cites style edit

--- src/renderer/viewCites.js
+++ src/renderer/viewCites.js
@@ -262,7 +262,8 @@ function renderSearch(cites) {
                       <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                   </svg>
               </div>
-              <h1 class="mt-3 text-lg text-gray-600 dark:text-gray-500">No hay resultados para "${searchCite.value}"</h1>
+              <h1 class="mt-3 text-lg text-gray-600 dark:text-gray-500">No se encuentran citas</h1>
+              <p class="mt-2 text-gray-400 dark:text-gray-400">La busqueda "${searchCite.value}" no encuentra ninguna cita.</p>
               <div class="flex items-center mt-4 sm:mx-auto gap-x-3">
                   <button class="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
